1db5d3e2930ff934a77cc8f77e34a7ee55ee9c5a
FrictionDuel <vegemoto.koemene.ch@gmail.com>
2025-11-28 18:02:09 +0900

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+const CACHE_NAME = 'friction-duel-cache-v1';
+const FILES_TO_CACHE = ['/', '/index.html', '/style.css', '/main.js', '/manifest.json'];
+
+self.addEventListener('install', evt => {
+  evt.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', evt => {
+  evt.respondWith(caches.match(evt.request).then(resp => resp || fetch(evt.request)));
+});
